31b578767ff92df2b7faf79c198d41516e310f5b
mulgir-govind <134280192+mulgir-govind@users.noreply.github.com>
2024-02-02 02:44:45 +0530

    [Series01][Part01][Problem01] Print rectangular start pattern (#1)
    Solved the following problem statement:
      Sam is making a forest visualizer. An N-dimensional forest is represented by
      the pattern of size NxN filled with ‘*’.
    
      For every value of ‘N’, help sam to print the corresponding N-dimensional forest.
    
    ---
      Example:
      Input: ‘N’ = 3
    
      Output:
    ```
      * * *
      * * *
      * * *
    ```
    
    ---
    References:
    - https://takeuforward.org/pattern/pattern-1-rectangular-star-pattern/
    - https://www.codingninjas.com/studio/problems/n-forest_6570177?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems

--- DSAProblemSolving/Series/S01-PatternPrinting/Part01/S01_P01_P01_VerticalStarPattern.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-class S01_P01_P01_VerticalStartPattern: ProblemProtocol {
-    let identifier = "S01_P01_P01"
-
-    func solve() {
-        print("Solving \(identifier)")
-    }
-}
--- DSAProblemSolving/Series/S01-PatternPrinting/Part01/S01_P01_P02_HorizontalStartPattern.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-class S01_P01_P02_HorizontalStartPattern: ProblemProtocol {
-    let identifier = "S01_P01_P02"
-
-    func solve() {
-        print("Solving \(identifier)")
-    }
-}
